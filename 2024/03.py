a640db75c1e2b85d9e599794bb993024ddcc0a0a
Lara Filippone <lara.filippone@gmail.com>
2024-12-21 13:18:48 +0100

    day 3 part two - add new regex to include do() and don't() and add check

--- 2024/03.py
+++ 2024/03.py
@@ -7,5 +7,29 @@ project_path = os.path.dirname(os.path.abspath((__file__)))
 with open(project_path + "/input/03", 'r') as content:
     text = content.read()
 
+
+# First part
 multiplications = re.findall(r"mul\((\d+),\s*(\d+)\)", text)
 total_result = sum(int(tuple[0]) * int(tuple[1]) for tuple in multiplications)
+print(total_result)  # 184511516
+
+# Second part
+enabled = True  # Status to check if the multiplication is to be considered or not
+total_result = 0
+regex = r"(do\(\)|don\'t\(\)|mul\((\d+),\s*(\d+)\))"
+
+multiplications = re.finditer(regex, text)
+for match in multiplications:
+    regex_match = match.group(0)
+
+    if regex_match == "do()":
+        enabled = True
+    elif regex_match == "don't()":
+        enabled = False
+    else:
+        if enabled:
+            x = int(match.group(2))
+            y = int(match.group(3))
+            total_result += x * y
+
+print(total_result)  # 90044227
